c0716717bcca97a891b19f9ffd2485d461ed2f63
yujinNa010 <borina010@gmail.com>
2025-08-26 21:29:50 +0900

    Update model.js

--- pages/api/model.js
+++ pages/api/model.js
@@ -1,10 +1,15 @@
 import fs from "fs";
 import path from "path";
 
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "public", "model.glb");
-  const fileBuffer = fs.readFileSync(filePath);
+export default async function handler(req, res) {
+  try {
+    const filePath = path.join(process.cwd(), "public", "model.glb");
+    const fileBuffer = fs.readFileSync(filePath);
 
-  res.setHeader("Content-Type", "model/gltf-binary");
-  res.send(fileBuffer);
+    res.setHeader("Content-Type", "model/gltf-binary");
+    res.status(200).send(fileBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to load model" });
+  }
 }
