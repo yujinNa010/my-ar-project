330b5a3054069809ade0f544f97d395dfd922f46
yujinNa010 <borina010@gmail.com>
2025-08-26 21:32:39 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,28 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*\\.glb",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "model/gltf-binary",
-          },
-        ],
-      },
-      {
-        source: "/:path*\\.usdz",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "model/vnd.usdz+zip",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
